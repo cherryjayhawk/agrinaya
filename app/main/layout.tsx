e7913807ca26899883af51f736b7030bc75a459b
cherryjayhawk <muhbintangr19@gmail.com>
2025-12-08 00:58:44 +0700

    fix: update layout background color for improved visibility

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="relative min-h-dvh flex flex-col text-sm text-foreground max-w-lg mx-auto border-2 border-black">
+    <div className="relative min-h-dvh flex flex-col text-sm text-foreground max-w-lg mx-auto bg-gray-100">
       <div className="relative">{children}</div>
       <div className="relative">
         <BottomNav />
